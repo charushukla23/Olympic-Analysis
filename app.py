94bd47dc9f97e8eb0bdf92d51071304c6013c391
Charu Shukla <74851712+charushukla23@users.noreply.github.com>
2026-02-26 16:45:55 +0530

    Comment out CSV loading and preprocessing
    Comment out data loading and preprocessing lines in app.py.

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-# df = pd.read_csv('athlete_events.csv')
-# region_df = pd.read_csv('noc_regions.csv')
+
 
 @st.cache_data
 def load_data():
@@ -18,7 +17,10 @@ def load_data():
 
 df = load_data()
 
-df = preprocessor.preprocess(df,region_df)
+# df = pd.read_csv('athlete_events.csv')
+# region_df = pd.read_csv('noc_regions.csv')
+
+# df = preprocessor.preprocess(df,region_df)
 
 st.sidebar.title("Olympics Analysis")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')

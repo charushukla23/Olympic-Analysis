17844329f7c6a32cfb49972df8825c03597758e3
Charu Shukla <74851712+charushukla23@users.noreply.github.com>
2026-02-26 16:40:56 +0530

    Implement data loading function with caching
    Refactor data loading into a separate function with caching.

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df = pd.read_csv('athlete_events.csv')
-region_df = pd.read_csv('noc_regions.csv')
+# df = pd.read_csv('athlete_events.csv')
+# region_df = pd.read_csv('noc_regions.csv')
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv('athlete_events.csv')
+    region_df = pd.read_csv('noc_regions.csv')
+    df = preprocessor.preprocess(df, region_df)
+    return df
+
+df = load_data()
 
 df = preprocessor.preprocess(df,region_df)
 
